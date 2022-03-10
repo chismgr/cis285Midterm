2afde6d21ef97e87e6a53585a7097291689f61a1
Garrett Chism <chismgr@umich.edu>
2022-03-10 15:05:27 -0500

    git commit -m "Updating"

--- AddIntegers.java
+++ AddIntegers.java
@@ -2,13 +2,8 @@ public class AddIntegers {
 
     public static void main(String[] args) {
 
-<<<<<<< HEAD
-        int first = 13;
-        int second = 3;
-=======
-        int first = 153;
-        int second = 253;
->>>>>>> d02f5e134498ab2c793bc782ac35afea6cd16422
+        int first = 133;
+        int second = 33;
 
         System.out.println("Enter two numbers: " + first + " " + second);
         int sum = first + second;
